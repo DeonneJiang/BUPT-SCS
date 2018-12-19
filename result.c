cbd2ba9f6eeb2dba9cbd8c409dc65eb9805b1638
Deonne <43706387+DeonneJiang@users.noreply.github.com>
2018-12-19 18:14:41 +0800

    Add files via upload

---
+++ result.c
@@ -0,0 +1,96 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define bool int
+#define true 1
+#define false 0
+
+typedef struct Date {
+char k1;
+int k2;
+struct k3 {
+char k31;
+int k32;
+struct k33 {
+char k331;
+int k332;
+}k33;
+int k34;
+}k3;
+int k4;
+}Date;
+
+int a[31],a1[31],a11[31];
+
+int a2[11];
+
+int x,y,z;
+
+double sd,asd,eqqq;
+
+Date D1;
+
+
+struct kkk {
+char k1;
+int k2;
+struct k3 {
+char k31;
+int k32;
+struct k33 {
+char k331;
+int k332;
+}k33;
+int k34;
+}k3;
+int k4;
+}kkk;
+
+
+void pro1(int a,double b)
+{
+const double CONPRO1=1;
+const double s=2.01859e-316;
+int t;
+
+char s;
+s = 1;
+writeln(1,2,3);
+
+}
+int func2(int a,int b)
+{
+const double SONF=12;
+for(x=1;<9;x++)
+{y = 1;}
+if(x == 1){
+y = 3;
+z = 123 - 23;
+
+}
+while( x == 1 ){
+y = 3;
+z = 123 - 23;
+
+}
+
+s = 1;
+kkk.k3.k33.k333 = 1;
+D1.k3.k33.k332 = 1;
+return x + y + z;
+
+}
+int main()
+{
+kkk.k3.k33.k332 = 1;
+kkk.k3.k33.k333 = 1;
+D1.k3.k33.k332 = 1;
+D1.k3.k33.k333 = 1;
+a2[2] = 13 + 12 * 123 / 123;
+pro1(1,2);
+pro1(1,2,3);
+pro(1,23);
+func2(2,2);
+
+return 0;
+}
